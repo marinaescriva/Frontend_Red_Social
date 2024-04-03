4423d570264ab51e09d9f09305de771bcb325873
marinaescriva <marinaescriva.24@gmail.com>
2024-04-03 19:56:08 +0200

    update: header redirections done

--- src/common/Clink/Clink.jsx
+++ src/common/Clink/Clink.jsx
@@ -1,7 +1,7 @@
 import "./Clink.css";
 import { useNavigate } from "react-router-dom";
 
-export const Clink = {(path, title)} => {
+export const Clink = ({path, title})=> {
 
     const navigate = useNavigate()
 
