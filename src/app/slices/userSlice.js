1c19b67409131f0f9360707df2cd767dab9c10ea
marinaescriva <marinaescriva.24@gmail.com>
2024-04-04 10:08:31 +0200

    feat: store slice added

---
+++ src/app/slices/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+
+    name:"user",
+    initialState: {
+        Credentials: {}
+    },
+    reducers: {
+        login: (state,action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+        logout:(state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+    }
+});
+
+export const { login, logout} = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice;
---
+++ src/app/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import {persistReducer} from "redux-persist";
+import { thunk } from "redux-thunk";
+
+
+import userSlice from "./slices/userSlice"; 
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers ({ //lo unico cambiable es añadir aqui
+    user: userSlice,
+});
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+ export default configureStore({
+    reducer: persistedReducer,
+    middleware:(getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: false,
+    }).concat(thunk),
+    });
+
+
+ })
