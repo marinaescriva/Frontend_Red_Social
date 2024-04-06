81470e41a8c2e01beab4e9c665ed887bc1d99bfe
marinaescriva <marinaescriva.24@gmail.com>
2024-04-06 13:46:49 +0200

    feat: edit profile view, design the structure

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,8 +1,23 @@
 import './Feed.css';
+// import { Clink } from '../Clink/Clink'
 
 
 export const Feed = () => {
   return (
-    <div className="feed-design">Soy el feed</div>
+    <>
+      <div className="feed-design">
+
+      <div className='feed-pannel'>
+        <div className='feed-user'>Carla</div>
+      {/* QUIERO AÑADIR EL NOMBRE DE LOS USUARIOS DE ESE POST EN CONCRETO <Clink path="/profile" title={rdxUser?.credentials?.user?.name} ></Clink> */}
+        <div className='feed-img'></div>
+        
+        <div></div>
+      </div>
+      <div className='feed-pannel'></div>
+      <div className='feed-pannel'></div>
+
+      </div>
+    </>
   );
 }
